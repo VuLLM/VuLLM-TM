9bb25112b195a1cea963552737fbd51bb08f94bb
VuLLM <vullmvullm2@gmail.com>
2025-03-26 11:20:18 +0200

    Update Fine_tuning_one_GPU.py

--- Injection_model/CodeQwen/Fine_tuning_one_GPU.py
+++ Injection_model/CodeQwen/Fine_tuning_one_GPU.py
@@ -7,7 +7,6 @@ import evaluate
 import os
 # import ..utils
 import sys
-sys.path.append('/sise/home/urizlo/VuLLM')
 from utils import Nxcode_7B, Create_lora_starCoder, Custom_SFTTrainer
 from Injection_model import Prepare_dataset_with_unique_lines
 import argparse
@@ -103,8 +102,6 @@ def main(path_trainset, path_testset, is_vulgen, output_dir, learning_rate, per_
         return result
 
     # config env varibles
-    os.environ["NEPTUNE_API_TOKEN"] = 'eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI4Y2VlNTFhZC1hODJkLTQ4NzItOTE0MS0yZmNkNWY3ZWE0MTEifQ=='
-    os.environ["NEPTUNE_PROJECT"] = 'zlotman/Localization-model'
     os.environ["NCCL_P2P_DISABLE"] = "1"
     os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
